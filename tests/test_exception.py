b1bcad8a9ed86f57dcbd7fd31196b25f1cff21c0
Nahuel <nahuel@getcollate.io>
2023-03-08 09:56:03 +0100

    Bump version + minor change (#7)
    * Bump version + minor change
    
    * Fix tests

--- tests/test_exception.py
+++ tests/test_exception.py
@@ -15,9 +15,11 @@ def test_select_without_table():
 
 def test_unsupported_query_type_in_sqlfluff():
     with pytest.raises(UnsupportedStatementException):
-        LineageRunner("WRONG SELECT FROM tab1")._eval()
+        LineageRunner("WRONG SELECT FROM tab1", dialect="ansi")._eval()
 
 
 def test_partial_unparsable_query_in_sqlfluff():
     with pytest.raises(InvalidSyntaxException):
-        LineageRunner("SELECT * FROM tab1 AS FULL FULL OUTER JOIN tab2")._eval()
+        LineageRunner(
+            "SELECT * FROM tab1 AS FULL FULL OUTER JOIN tab2", dialect="ansi"
+        )._eval()
