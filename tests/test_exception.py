3a7e271b539019e51e7e43f5bc3f998db24ce45d
Nahuel Verdugo Revigliono <nahuel@getcollate.io>
2023-04-03 10:47:14 +0200

    Update with changes done in sqllineage v1.4.1

--- tests/test_exception.py
+++ tests/test_exception.py
@@ -1,3 +1,5 @@
+import warnings
+
 import pytest
 
 from sqllineage.exceptions import (
@@ -13,8 +15,8 @@ def test_select_without_table():
         LineageRunner("select * from where foo='bar'")._eval()
 
 
-def test_unsupported_query_type_in_sqlfluff():
-    with pytest.raises(UnsupportedStatementException):
+def test_full_unparsable_query_in_sqlfluff():
+    with pytest.raises(InvalidSyntaxException):
         LineageRunner("WRONG SELECT FROM tab1", dialect="ansi")._eval()
 
 
@@ -23,3 +25,17 @@ def test_partial_unparsable_query_in_sqlfluff():
         LineageRunner(
             "SELECT * FROM tab1 AS FULL FULL OUTER JOIN tab2", dialect="ansi"
         )._eval()
+
+
+def test_unsupported_query_type_in_sqlfluff():
+    with pytest.raises(UnsupportedStatementException):
+        LineageRunner(
+            "CREATE UNIQUE INDEX title_idx ON films (title)", dialect="ansi"
+        )._eval()
+
+
+def test_deprecated_warning_in_sqlparse():
+    with warnings.catch_warnings(record=True) as w:
+        LineageRunner("SELECT * FROM DUAL", dialect="non-validating")._eval()
+        assert len(w) == 1
+        assert issubclass(w[0].category, DeprecationWarning)
