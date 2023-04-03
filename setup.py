afb1a602352b6e4535599a1308dbe4b721ce4e4f
Nahuel <nahuel@getcollate.io>
2023-04-03 12:44:36 +0200

    Bump version + update sqlfluff dependency version (#9)

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     install_requires=[
         "sqlparse>=0.3.1",
         "networkx>=2.4",
-        "sqlfluff==2.0.0a6",
+        "sqlfluff==2.0.2",
     ],
     entry_points={"console_scripts": ["sqllineage = sqllineage.cli:main"]},
     extras_require={
