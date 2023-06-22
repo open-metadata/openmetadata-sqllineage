f4a2e890eef7ce14beecf875108a856850d79019
ulixius9 <mayursingal9@gmail.com>
2023-06-22 11:22:09 +0530

    fix network version

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     python_requires=">=3.7",
     install_requires=[
         "sqlparse==0.4.3",
-        "networkx>=2.4",
+        "networkx==3.0",
         "sqlfluff==2.0.2",
     ],
     entry_points={"console_scripts": ["sqllineage = sqllineage.cli:main"]},
